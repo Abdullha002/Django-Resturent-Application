ce2f5eea2e81b496474b5ca5cb0c5aaeb0cc2bbd
Abdullha002 <abdullahmulla002@gmail.com>
2025-12-09 10:47:40 +0530

    Updated to show the error messages and Registration success message

--- Food_Main/accounts/views.py
+++ Food_Main/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from . forms import UserForm
 from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -26,6 +27,7 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = user.CUSTOMER
             user.save()
+            messages.success(request, "Your account has been registred successfully")
 
             return redirect('registerUser')
     else:
