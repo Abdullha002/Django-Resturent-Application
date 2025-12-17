efcb437b953f38af65dc4954f10ef0e1175ae01f
Abdullha002 <abdullahmulla002@gmail.com>
2025-12-17 12:54:03 +0530

    Created a menu app,  created category , foogitem models and migrated to database

--- Food_Main/Food_Main/settings.py
+++ Food_Main/Food_Main/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'vendor'
+    'vendor',
+    'menu'
 ]
 
 MIDDLEWARE = [
